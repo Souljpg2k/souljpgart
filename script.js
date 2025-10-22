88de852728304523d7d75ca56023bf0f9ee52c8e
souljpg <crystalsoul2k@gmail.com>
2025-10-23 02:22:07 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+const galleryData = [
+    { title: "Torii", description: "Arknights", img: "img/221068.png" },
+    { title: "CS001", description: "Crime Scene", img: "img/CSJPG.jpg" },
+    { title: "Laboratory", description: "LB001", img: "img/lapart.jpg" },
+    { title: "Dream", description: "My Dream", img: "img/dream542568.jpg" },
+];
+
+const gallery = document.getElementById('gallery');
+
+galleryData.forEach(item => {
+    const card = document.createElement('div');
+    card.className = 'gallery-item';
+    card.innerHTML = `
+        <img src="${item.img}" alt="${item.title}" />
+        <div class="caption">
+          <h3 class="title">${item.title}</h3>
+          <p>${item.description}</p>
+        </div>
+    `;
+    gallery.appendChild(card);
+});
+
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+            observer.unobserve(entry.target);
+        }
+    });
+}, { threshold: 0.1 });
+
+document.querySelectorAll('.gallery-item').forEach(item => {
+    observer.observe(item);
+});
+
+const backToTopBtn = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+        backToTopBtn.classList.add("show");
+    } else {
+        backToTopBtn.classList.remove("show");
+    }
+});
+
+backToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+const menuBtn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle("show");
+    menuBtn.classList.toggle("open");
+});
