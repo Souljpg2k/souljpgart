f88a78237ffc37768c897049190b2768d6d52600
souljpg <crystalsoul2k@gmail.com>
2026-03-15 20:06:36 +0700

    🐳 Scale > 1.3 , duration - 1

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -10,9 +10,9 @@ function Background({ ref }: BackgroundProps) {
         <div ref={ref} className='w-full h-full relative rounded-[20px] overflow-hidden '>
             <motion.img
                 className='w-full h-full object-cover pointer-events-none'
-                initial={{ opacity: 0, scale: 1.1, filter: 'blur(8px)' }}
+                initial={{ opacity: 0, scale: 1.3, filter: 'blur(8px)' }}
                 animate={{ opacity: 1, scale: 1, filter: 'blur(0)' }}
-                transition={{ duration: 2 }}
+                transition={{ duration: 1 }}
                 src={bg}
                 alt='background'
             />
