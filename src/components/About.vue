d6d07687e6eac85aa3b1ce5d608b39f285301ae4
souljpg <crystalsoul2k@gmail.com>
2026-09-20 20:41:35 +0700

    add about component

---
+++ src/components/About.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { motion } from "motion-v";
+</script>
+
+<template>
+	<motion.div
+		:initial="{ opacity: 0, scale: 0.8 }"
+		:animate="{ opacity: 1, scale: 1 }"
+		:transition="{ duration: 4, ease: 'anticipate' }"
+		class="hidden md:grid fixed m-7 bottom-0 font-dm text-lg"
+	>
+		<p>illustrator</p>
+		<p>character design</p>
+	</motion.div>
+</template>
